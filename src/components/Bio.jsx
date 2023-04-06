470ad3390a5c089c4129e02c98d8bfc978eb8175
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-04-07 11:46:58 +1200

    Add modals and hrefs

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,9 +1,38 @@
+import React, { useState, useRef } from 'react'
 import logo from '../assets/hmfavicon.png'
+
+import './Bio.css'
 
 const Bio = () => {
   const userDetails = {
     name: 'Halmai McRae',
     about: 'The things I like.',
+    linkedin:
+      'https://www.linkedin.com/in/halmai-mcrae/',
+    github: 'https://github.com/halmai-mcrae',
+    website: 'https://halmaimcrae.co.nz',
+    strava:
+      'https://www.strava.com/athletes/112599020',
+    medium: 'https://medium.com/@halmai-mcrae',
+  }
+
+  const [showModal, setShowModal] =
+    useState(false)
+
+  const inputRef = useRef(null)
+
+  const handleFollowClick = () => {
+    setShowModal(true)
+  }
+
+  const handleModalClose = () => {
+    setShowModal(false)
+    const followButton = document.querySelector(
+      '.follow-button'
+    )
+    if (followButton) {
+      followButton.checked = false
+    }
   }
 
   return (
@@ -14,7 +43,62 @@ const Bio = () => {
       <div className="profile-info">
         <h1>{userDetails.name}</h1>
         <p>{userDetails.about}</p>
+        <input
+          className="follow-button"
+          type="checkbox"
+          onClick={handleFollowClick}
+          ref={inputRef}
+        />
       </div>
+
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h3>Links</h3>
+            <div class="social-container">
+              <a
+                href={userDetails.github}
+                target="_blank"
+                class="social"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <a
+                href={userDetails.strava}
+                target="_blank"
+                class="social"
+              >
+                <i class="fab fa-strava"></i>
+              </a>
+              <a
+                href={userDetails.medium}
+                target="_blank"
+                class="social"
+              >
+                <i class="fab fa-medium"></i>
+              </a>
+              <a
+                href={userDetails.linkedin}
+                target="_blank"
+                class="social"
+              >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <a
+                href={userDetails.website}
+                target="_blank"
+                class="social"
+              >
+                <i class="fas fa-heart"></i>
+              </a>
+            </div>
+
+            <button onClick={handleModalClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,65 @@
+import React, { useState } from 'react'
+
+import './Nav.css'
+
 const Nav = () => {
+  const [showAlert, setShowAlert] =
+    useState(false)
+
+  const handleHeartClick = () => {
+    setShowAlert(true)
+  }
+
+  const handleAlertClose = () => {
+    setShowAlert(false)
+  }
+
   return (
     <nav>
       <button class="logo">Antigram</button>
       <input
         type="text"
         className="search"
-        placeholder="I'm looking for..."
+        placeholder="Search"
       />
       <span className="nav-links">
         <button>
           <i className="fas fa-home" />
         </button>
         <button>
-          <i className="far fa-comment-alt" />
+          <a
+            href="mailto:hello@halmaimcrae.co.nz"
+            target="_blank"
+          >
+            <i className="far fa-comment-alt" />
+          </a>
         </button>
         <button>
-          <i className="far fa-compass" />
+          <a
+            href="https://www.google.com/maps/place/Wellington/@-41.2529601,174.7542577,11z/data=!3m1!4b1!4m6!3m5!1s0x6d38b1fc49e974cb:0xa00ef63a213b470!8m2!3d-41.2923814!4d174.7787463!16zL20vMDg1M2c"
+            target="_blank"
+          >
+            <i className="far fa-compass" />
+          </a>
         </button>
-        <button>
+        <button onClick={handleHeartClick}>
           <i className="far fa-heart" />
         </button>
       </span>
+
+      {showAlert && (
+        <div className="alert-modal">
+          <div className="alert-content">
+            <h3>
+              Thanks, but your likes mean nothing
+              here.
+            </h3>
+            <button onClick={handleAlertClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
